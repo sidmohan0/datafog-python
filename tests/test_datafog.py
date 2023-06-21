8b5a0c17d0fe80382d9be3857a80572690a79236
Sid Mohan <sidmohan001@gmail.com>
2023-06-21 16:42:08 -0700

    unit test for scan

--- tests/test_datafog.py
+++ tests/test_datafog.py
@@ -5,12 +5,12 @@ def test_scan():
     datafog = DataFog()
 
     # Test case: When the csv file has columns present in predefined_header_values
-    contains_pii, pii_fields = datafog.scan("test.csv")
+    contains_pii, pii_fields = datafog.scan("files/test.csv")
     assert contains_pii == True
     assert set(pii_fields) == {"name", "age", "email_address"}
 
     # Test case: When the csv file does not have any column present in predefined_header_values
     # Assuming another csv file "test_no_pii.csv" with columns not in predefined_header_values
-    contains_pii, pii_fields = datafog.scan("test_no_pii.csv")
+    contains_pii, pii_fields = datafog.scan("files/test_no_pii.csv")
     assert contains_pii == False
     assert pii_fields == []
